2bd8c52f9a2a25c4aec18a67be53145cd94050ab
samiam44 <137422820+samiam44@users.noreply.github.com>
2026-07-23 23:39:19 -0400

    Create STIG-ID-WN10-AU-000500.ps1

---
+++ STIGS/STIG-ID-WN10-AU-000500.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : John Samudio
+    LinkedIn        : www.linkedin.com/in/johnsamudio
+    GitHub          : https://github.com/samiam44
+    Date Created    : 2026-07-24
+    Last Modified   : 2026-07-24
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000500
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-AU-000500/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# YOUR CODE GOES HERE
+
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
+$valueName = "MaxSize"
+$valueData = 0x00008000  # 32768 in decimal (32 KB)
+
+# Create the registry path if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+    Write-Host "Created registry path: $regPath"
+}
+
+# Set the DWORD value (New-ItemProperty creates it if missing, updates it if present)
+New-ItemProperty -Path $regPath -Name $valueName -Value $valueData -PropertyType DWord -Force | Out-Null
+
+Write-Host "Set $valueName to $valueData (0x$($valueData.ToString('X8'))) at $regPath"
+
